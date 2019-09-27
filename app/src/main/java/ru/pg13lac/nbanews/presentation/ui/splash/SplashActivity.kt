f32d6b3f67089e0c339da9de149c5e04b82e6619
Andrey2K17 <nba.grinin.96@gmail.com>
2019-09-27 13:21:00 +0700

    add nav_graph, fix splashScreen

--- app/src/main/java/ru/pg13lac/nbanews/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/ru/pg13lac/nbanews/presentation/ui/splash/SplashActivity.kt
@@ -1,13 +1,17 @@
 package ru.pg13lac.nbanews.presentation.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.pg13lac.nbanews.R
+import ru.pg13lac.nbanews.presentation.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
