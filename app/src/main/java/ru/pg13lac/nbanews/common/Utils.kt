f067f89a8c9354946609659019689b5f754d72c2
Andrey2K17 <nba.grinin.96@gmail.com>
2019-10-11 13:31:59 +0700

    fix Utils

--- app/src/main/java/ru/pg13lac/nbanews/common/Utils.kt
+++ app/src/main/java/ru/pg13lac/nbanews/common/Utils.kt
@@ -5,11 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.bumptech.glide.request.RequestOptions
-import kotlinx.android.synthetic.main.game_leaders.view.*
 import ru.pg13lac.nbanews.R
-import ru.pg13lac.nbanews.domain.entity.GameLeaders
-import ru.pg13lac.nbanews.domain.entity.CommonTypeStats
-import ru.pg13lac.nbanews.domain.entity.GameLeadersType
 
 fun ViewGroup.inflate(layoutRes: Int): View =
     LayoutInflater.from(context).inflate(layoutRes, this, false)
@@ -27,40 +23,4 @@ fun setTeamImage(team: String, imageView: ImageView) {
         .load("https://stats.nba.com/media/img/teams/logos/${team}_logo.svg")
         .centerCrop()
         .into(imageView)
-}
-
-fun setGameLeadersInView(view: View, gameLeaders: List<GameLeaders>, type: CommonTypeStats) {
-    val playerStats = mutableListOf<GameLeadersType>()
-    var statsCategory = ""
-    when (type) {
-        CommonTypeStats.POINT -> {
-            for (i in 0..1) {
-                playerStats.add(gameLeaders[i].player_pts)
-            }
-            statsCategory = view.context.getString(R.string.point_category)
-        }
-        CommonTypeStats.REBOUND -> {
-            for (i in 0..1) {
-                playerStats.add(gameLeaders[i].player_reb)
-            }
-            statsCategory = view.context.getString(R.string.rebound_category)
-        }
-        CommonTypeStats.ASSIST -> {
-            for (i in 0..1) {
-                playerStats.add(gameLeaders[i].player_ast)
-            }
-            statsCategory = view.context.getString(R.string.assist_category)
-        }
-    }
-    with(view) {
-        tvLeftPlayerName.text = playerStats[1].name
-        tvRightPlayerName.text = playerStats[0].name
-        tvLeftPlayerAmount.text = playerStats[1].amount
-        tvRightPlayerAmount.text = playerStats[0].amount
-        tvLeftTeam.text = gameLeaders[1].team_name
-        tvRightTeam.text = gameLeaders[0].team_name
-        setPlayerImage(playerStats[1].id, ivLeftPlayer)
-        setPlayerImage(playerStats[0].id, ivRightPlayer)
-        tvStatsCategory.text = statsCategory
-    }
 }
