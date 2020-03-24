8cb3c5b619e5f25ade285e6526006fc949df3e11
Andrey2K17 <nba.grinin.96@gmail.com>
2020-03-24 20:02:52 +0700

    создание фрагмента для любимых команд

--- app/src/main/java/ru/pg13lac/nbanews/di/AppModule.kt
+++ app/src/main/java/ru/pg13lac/nbanews/di/AppModule.kt
@@ -4,6 +4,7 @@ import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
 import ru.pg13lac.nbanews.presentation.ui.box_score.BoxScoreFragment
+import ru.pg13lac.nbanews.presentation.ui.favorite_team.FavoriteTeamFragment
 import ru.pg13lac.nbanews.presentation.ui.game_details.GameDetailsFragment
 import ru.pg13lac.nbanews.presentation.ui.game_list.GameListFragment
 import ru.pg13lac.nbanews.presentation.ui.main.MainActivity
@@ -33,4 +34,7 @@ interface AppModule {
 
     @ContributesAndroidInjector
     fun teamsTableFragmentInjection(): TeamsTableFragment
+
+    @ContributesAndroidInjector
+    fun favoriteTeamFragmentInjection(): FavoriteTeamFragment
 }
---
+++ app/src/main/java/ru/pg13lac/nbanews/presentation/ui/favorite_team/FavoriteTeamFragment.kt
@@ -0,0 +1,8 @@
+package ru.pg13lac.nbanews.presentation.ui.favorite_team
+
+import ru.pg13lac.nbanews.R
+import ru.pg13lac.nbanews.presentation.ui.base.BaseFragment
+
+class FavoriteTeamFragment : BaseFragment() {
+    override val layoutRes = R.layout.fragment_favorite_team
+}
