ace5fc751661c470ac421cb2762afca591bfb22e
Andrey2K17 <nba.grinin.96@gmail.com>
2020-04-12 20:52:16 +0700

    создание списка команд

--- app/src/main/java/ru/pg13lac/nbanews/common/const.kt
+++ app/src/main/java/ru/pg13lac/nbanews/common/const.kt
@@ -32,3 +32,38 @@ val nameTeams = mapOf(
     "1610612762" to "Jazz",
     "1610612764" to "Wizards"
 )
+
+data class TeamNameWithId(val id: String, val name: String, val conf: String, val ref: String)
+
+val teamList = listOf<TeamNameWithId>(
+    TeamNameWithId("583ecb8f-fb46-11e1-82cb-f4ce4684ea4c", "Атланта Хоукс", "east", "atl"),
+    TeamNameWithId("583eccfa-fb46-11e1-82cb-f4ce4684ea4c", "Бостон Селтикс", "east", "bos"),
+    TeamNameWithId("583ec9d6-fb46-11e1-82cb-f4ce4684ea4c", "Бруклин Нетс", "east", "bkn"),
+    TeamNameWithId("583ec8d4-fb46-11e1-82cb-f4ce4684ea4c", "Вашингтон Визардз", "east", "was"),
+    TeamNameWithId("583ec928-fb46-11e1-82cb-f4ce4684ea4c", "Детройт Пистонс", "east", "det"),
+    TeamNameWithId("583ec7cd-fb46-11e1-82cb-f4ce4684ea4c", "Индиана Пэйсерс", "east", "ind"),
+    TeamNameWithId("583ec773-fb46-11e1-82cb-f4ce4684ea4c", "Кливленд Кавальерс", "east", "cle"),
+    TeamNameWithId("583ecea6-fb46-11e1-82cb-f4ce4684ea4c", "Майами Хит", "east", "mia"),
+    TeamNameWithId("583ecefd-fb46-11e1-82cb-f4ce4684ea4c", "Милуоки Бакс", "east", "mil"),
+    TeamNameWithId("583ec70e-fb46-11e1-82cb-f4ce4684ea4c", "Нью-Йорк Никс", "east", "nyk"),
+    TeamNameWithId("583ed157-fb46-11e1-82cb-f4ce4684ea4c", "Орландо Мэджик", "east", "orl"),
+    TeamNameWithId("583ecda6-fb46-11e1-82cb-f4ce4684ea4c", "Торонто Рапторс", "east", "tor"),
+    TeamNameWithId("583ec87d-fb46-11e1-82cb-f4ce4684ea4c", "Филадельфия  76ерс", "east", "phi"),
+    TeamNameWithId("583ec5fd-fb46-11e1-82cb-f4ce4684ea4c", "Чикаго Буллз", "east", "chi"),
+    TeamNameWithId("583ec97e-fb46-11e1-82cb-f4ce4684ea4c", "Шарлотт Хорнетс", "east", "cha"),
+    TeamNameWithId("583ec825-fb46-11e1-82cb-f4ce4684ea4c", "Голден Стэйт Уорриорз", "west", "gsw"),
+    TeamNameWithId("583ecf50-fb46-11e1-82cb-f4ce4684ea4c", "Даллас Маверикс", "west", "dal"),
+    TeamNameWithId("583ed102-fb46-11e1-82cb-f4ce4684ea4c", "Денвер Наггетс", "west", "den"),
+    TeamNameWithId("583ecdfb-fb46-11e1-82cb-f4ce4684ea4c", "Лос-Анджелес Клипперс", "west", "lac"),
+    TeamNameWithId("583ecae2-fb46-11e1-82cb-f4ce4684ea4c", "Лос-Анджелес Лейкерс", "west", "lal"),
+    TeamNameWithId("583eca88-fb46-11e1-82cb-f4ce4684ea4c", "Мемфис Гриззлис", "west", "mem"),
+    TeamNameWithId("583eca2f-fb46-11e1-82cb-f4ce4684ea4c", "Миннесота Тимбервулвс", "west", "min"),
+    TeamNameWithId("583ecc9a-fb46-11e1-82cb-f4ce4684ea4c", "Нью-Орлеан Пеликанс", "west", "nop"),
+    TeamNameWithId("583ecfff-fb46-11e1-82cb-f4ce4684ea4c", "Оклахома Сити Тандер", "west", "okc"),
+    TeamNameWithId("583ed056-fb46-11e1-82cb-f4ce4684ea4c", "Портленд Трэйл Блэйзерс", "west", "por"),
+    TeamNameWithId("583ed0ac-fb46-11e1-82cb-f4ce4684ea4c", "Сакраменто Кингз", "west", "sac"),
+    TeamNameWithId("583ecd4f-fb46-11e1-82cb-f4ce4684ea4c", "Сан-Антонио Спёрс", "west", "sas"),
+    TeamNameWithId("583ecfa8-fb46-11e1-82cb-f4ce4684ea4c", "Финикс Санс", "west", "phx"),
+    TeamNameWithId("583ecb3a-fb46-11e1-82cb-f4ce4684ea4c", "Хьюстон Рокетс", "west", "hou"),
+    TeamNameWithId("583ece50-fb46-11e1-82cb-f4ce4684ea4c", "Юта Джаз", "west", "uta")
+)
